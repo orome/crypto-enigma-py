e958cd7b03d220d84ab40da0ba5c97c26f72fb6f
Roy Levien <royl@aldaron.com>
2015-11-03 21:13:19 -0500

    Expand command line support

--- enigma.py
+++ enigma.py
@@ -33,6 +33,11 @@ Show an Enigma machine configuration in the specified format, optionally
 indicating the encoding of a specified character.
 """
 
+# TBD - Formats arent all the same: encoding has two more, the encoding and the chunked encoding <<<
+# TBD - Combine encode and run, or keep seperate? <<<
+_DESC_ENCODE = """\
+Show the encoding of a message.
+"""
 # _EPILOG_DISPLAY = """\
 # The the number of times '-d' is supplied will determine the level
 # of detail displayed:
@@ -43,7 +48,7 @@ indicating the encoding of a specified character.
 #        processing stage
 # """
 
-_EPILOG_DISPLAY = """\
+_EPILOG_FORMAT_DISPLAY = """\
 The selection for '--format' will determine what is shown; options include:
     - 'internal', which will show a detailed schematic of each
       processing stage;
@@ -51,17 +56,26 @@ The selection for '--format' will determine what is shown; options include:
     - 'windows', just the letters visible at the windows; and
     - 'config', the default, which simply shows the specification
       of the configuration
+The program is forgiving about forgotten format values and will accept a range
+of reasonable substitutes (e.g., 'detailed' or 'schematic' for 'internal').
 """
-
-# TBD - Not needed if help can works when in parent
-_HELP_ARGS = ['--help', '-h', '-?']
-_HELP_KWARGS = {'action': 'help', 'help': 'show this help message and exit'}
-
+_EPILOG_DISPLAY = _EPILOG_FORMAT_DISPLAY
+_EPILOG_ENCODE = _EPILOG_FORMAT_DISPLAY
 
 # ASK - What's idiomatic?
 def fmt_arg(arg):
     return arg.upper()
     # return '<' + arg.lower() + '>'
+
+# TBD - Not needed if help can works when in parent
+_HELP_ARGS = ['--help', '-h', '-?']
+_HELP_KWARGS = {'action': 'help', 'help': 'show this help message and exit'}
+_CONFIG_ARGS = ['config']
+_CONFIG_KWARGS = {'metavar': fmt_arg('config'), 'action': 'store'}
+_DISPLAY_KWARGS = {'title': 'display arguments', 'description': 'optional display arguments'}
+_FORMAT_ARGS = ['--format', '-f']
+_FORMAT_KWARGS = {'metavar': fmt_arg('format'), 'action': 'store', 'nargs': '?', 'default': 'single', 'const': 'single'}
+
 
 if __name__ == '__main__':
 
@@ -73,40 +87,45 @@ if __name__ == '__main__':
     parent_parser.add_argument('--verbose', '-v',
                                action='store_true',
                                help='display additional information (may have no effect)')
-    parent_parser.add_argument('--version', '-V',
-                               action='version', version='%(prog)s {0}'.format(__version__),
-                               help='display package version number and exit')
+    # parent_parser.add_argument('--version', '-V',
+    #                            action='version', version='%(prog)s {0}'.format(__version__),
+    #                            help='display package version number and exit')
     # parent_parser.add_argument(*_HELP_ARGS, **_HELP_KWARGS)
+
+    # config_parser = argparse.ArgumentParser(add_help=False)
+    # # Almost works, but puts config in the wrong position and won't show command help unless theres a config!
+    # config_parser.add_argument('config', metavar=fmt_arg('config'),
+    #                             action='store',
+    #                             help='the machine configuration to %(dest)s')
 
     parser = argparse.ArgumentParser(description=_DESC, parents=[parent_parser],
                                      epilog=_EPILOG,
-                                     # usage = 'xyz',
+                                     #usage = 'enigma.py [<options>] COMMAND CONFIG',
                                      add_help=False,
                                      formatter_class=argparse.RawDescriptionHelpFormatter)
     parser.add_argument(*_HELP_ARGS, **_HELP_KWARGS)
 
-    subparsers = parser.add_subparsers(help='', dest='command',
-                                       title='required arguments',
-                                       description='description, some commands to choose from',
+    commands = parser.add_subparsers(help='', dest='command',
+                                     #title='required arguments',
+                                       #description='description, some commands to choose from',
                                        metavar=fmt_arg('command')
-                                       )
+                                     )
+    # parser.add_argument('config', metavar=fmt_arg('config'),
+    #                          action='store',
+    #                          help='the machine configuration to %(dest)s')
 
     # Display machine state
-    show_parser = subparsers.add_parser('show', parents=[parent_parser],
-                                        description=_DESC_DISPLAY,
-                                        epilog=_EPILOG_DISPLAY,
-                                        help='display an Enigma machine configuration',
-                                        add_help=False,
-                                        formatter_class=argparse.RawDescriptionHelpFormatter)
-    show_parser.add_argument('config', metavar=fmt_arg('config'),
-                             action='store',
-                             help='the machine configuration to show')
-    show_display_group = show_parser.add_argument_group(title="display arguments", description="some display arguments")
-    show_display_group.add_argument('--format', '-f', metavar=fmt_arg('format'),
-                                    # choices=('internal', 'single', 'config'),
-                                    action='store', nargs='?', default='single', const='single',
-                                    help='the format to use to show the configuration; see below')
-
+    show_parser = commands.add_parser('show', parents=[parent_parser],
+                                      description=_DESC_DISPLAY,
+                                      epilog=_EPILOG_DISPLAY,
+                                      help='display an Enigma machine configuration',
+                                      add_help=False,
+                                      formatter_class=argparse.RawDescriptionHelpFormatter)
+    _CONFIG_KWARGS['help'] = 'the machine configuration to show'
+    show_parser.add_argument(*_CONFIG_ARGS, **_CONFIG_KWARGS)
+    show_display_group = show_parser.add_argument_group(**_DISPLAY_KWARGS)
+    _FORMAT_KWARGS['help'] = 'the format to use to show the configuration; see below'
+    show_display_group.add_argument(*_FORMAT_ARGS, **_FORMAT_KWARGS)
     show_display_group.add_argument('--letter', '-l', metavar=fmt_arg('letter'),
                                     action='store', nargs='?', default='', const='',
                                     help='an optional input letter to highlight as it is processed by the '
@@ -116,20 +135,39 @@ if __name__ == '__main__':
     # group = parser.add_mutually_exclusive_group()
 
     # Encode a message
-    encode_parser = subparsers.add_parser('encode', help='encode a message', parents=[parent_parser], add_help=False)
+    encode_parser = commands.add_parser('encode', parents=[parent_parser],
+                                        description=_DESC_ENCODE,
+                                        epilog=_EPILOG_ENCODE,
+                                        help='encode a message',
+                                        add_help=False,
+                                        formatter_class=argparse.RawDescriptionHelpFormatter)
+    _CONFIG_KWARGS['help'] = 'the machine setup at the start of encoding'
+    encode_parser.add_argument(*_CONFIG_ARGS, **_CONFIG_KWARGS)
+    encode_parser.add_argument('message', metavar=fmt_arg('message'), action='store',
+                               help='a message to encode')
+    encode_display_group = encode_parser.add_argument_group(**_DISPLAY_KWARGS)
+    _FORMAT_KWARGS['help'] = 'the format to use to show each configuration; see below'
+    encode_display_group.add_argument(*_FORMAT_ARGS, **_FORMAT_KWARGS)
     encode_parser.add_argument(*_HELP_ARGS, **_HELP_KWARGS)
 
 
     # Show machine operation
-    run_parser = subparsers.add_parser('run', help='display machine operation', parents=[parent_parser], add_help=False)
+    run_parser = commands.add_parser('run', help='display machine operation', parents=[parent_parser], add_help=False)
+    _CONFIG_KWARGS['help'] = 'the machine configuration to start running from'
+    run_parser.add_argument(*_CONFIG_ARGS, **_CONFIG_KWARGS)
     run_parser.add_argument(*_HELP_ARGS, **_HELP_KWARGS)
 
     # Show machine operation
-    step_parser = subparsers.add_parser('step', help='step the configuration once', parents=[parent_parser], add_help=False)
-    step_parser.add_argument('config', metavar=fmt_arg('config'),
-                             action='store',
-                             help='the machine to start with')
+    step_parser = commands.add_parser('step', help='step the configuration once', parents=[parent_parser], add_help=False)
+    _CONFIG_KWARGS['help'] = 'the machine configuration to begin stepping from'
+    step_parser.add_argument(*_CONFIG_ARGS, **_CONFIG_KWARGS)
     step_parser.add_argument(*_HELP_ARGS, **_HELP_KWARGS)
+
+    # Just show the package version
+    _HELP_VERSION = 'Show the package version and exit'
+    version_parser = commands.add_parser('version', help=_HELP_VERSION.lower(), add_help=False,
+                                         description=_HELP_VERSION + '.')
+    version_parser.add_argument(*_HELP_ARGS, **_HELP_KWARGS)
 
 
     # try:
@@ -143,71 +181,65 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
+    _FMTS_INTERNAL = ['internal', 'detailed', 'schematic']
+    _FMTS_SINGLE = ['single', 'encoding', 'summary']
+    _FMTS_WINDOWS = ['windows', 'winds']
+    _FMTS_CONFIG = ['config', 'configuration', 'spec', 'specification']
+    _FMTS_DEBUG = ['debug']
+
     try:
-        cfg = EnigmaConfig.config_enigma_from_string(args.config)
-    except EnigmaError as e:
-        print(e.message)
-        exit(0)
-    else:
         # TBD - Add encoding note to config and windows (e.g with P > K) <<<
         # TBD - Add components format that lists the components and their attributes <<<
-        if args.command == 'show':
-            format = args.format
-            let = args.letter
-            if format in ['internal']:
-                print(cfg.config_string_internal(let))
-            elif format in ['single']:
-                print(cfg.config_string(let))
-            # # TBD - Another version that prints spec on individual lines; or list or elements?
-            elif format in ['windows']:
-                print(cfg.windows())
-            elif format in ['config']:
-                print(cfg)
-            # Hidden option for debugging
-            elif format in ['debug']:
-                print(cfg.__repr__())
+        if args.command == 'version':
+            print('{0}'.format(__version__))
+        else:
+            cfg = EnigmaConfig.config_enigma_from_string(args.config)
+            fmt = args.format
+            if fmt in _FMTS_INTERNAL + _FMTS_SINGLE +_FMTS_WINDOWS + _FMTS_CONFIG + _FMTS_DEBUG:
+                if args.command == 'show':
+                    let = args.letter
+                    if fmt in _FMTS_INTERNAL:
+                        print(cfg.config_string_internal(let))
+                    elif fmt in _FMTS_SINGLE:
+                        print(cfg.config_string(let))
+                    # TBD - Another version that prints spec on individual lines; or list or elements?
+                    elif fmt in _FMTS_WINDOWS:
+                        print(cfg.windows())
+                    elif fmt in _FMTS_CONFIG:
+                        print(cfg)
+                    # Hidden option for debugging
+                    elif fmt in _FMTS_DEBUG:
+                        print(cfg.__repr__())
+                elif args.command == 'encode':
+                    msg = args.message
+                    if fmt in _FMTS_INTERNAL:
+                        cfg.print_operation_internal(msg)
+                    elif fmt in _FMTS_SINGLE:
+                        cfg.print_operation(msg)
+                    # TBD - Another version that prints spec on individual lines; or list or elements?
+                    elif fmt in _FMTS_WINDOWS:
+                        print('TBD')
+                    elif fmt in _FMTS_CONFIG:
+                        print('TBD')
+                    # Hidden option for debugging
+                    elif fmt in _FMTS_DEBUG:
+                        print('TBD')
+                # TBD - Should share same arguments and same stucture with show <<<
+                elif args.command == 'step':
+                    print(cfg.step())
             else:
-                print('?')
-        # TBD - Should share same arguments and same stucture with show <<<
-        elif args.command == 'step':
-            print(cfg.step())
+                print('Bad argument - Unrecognized format, {0}'.format(fmt))
+                exit(1)
+
+    except EnigmaError as e:
+        print(e.message)
+        exit(1)
 
 
-# import argparse
-#
-# parser = argparse.ArgumentParser()
-#
-# # Put common subparser arguments here. Each sub parser will have
-# # its own -h option, so disable it on the shared base.
-# subbase = argparse.ArgumentParser(add_help=False)
-# subbase.add_argument('config', metavar='CONFIG', action='store', help='the config to use')
-#
-# subparsers = parser.add_subparsers(help='', dest='command',  metavar='COMMAND', title='required arguments',
-#                                    description='two arguments are required')
-#
-# # Add subbase to the parent list for each subparser.
-# cmda_parser = subparsers.add_parser('cmdA', parents=[subbase],  help='a first command')
-# cmdb_parser = subparsers.add_parser('cmdB', parents=[subbase], help='the second operation')
-# cmdc_parser = subparsers.add_parser('cmdC', parents=[subbase], help='yet another thing')
-#
+
+
 # print(parser.parse_args())
-
-# import argparse
-#
-# parser = argparse.ArgumentParser()
-#
-# subparsers = parser.add_subparsers(help='', dest='command',  metavar='COMMAND'
-# # title='required arguments',
-# #                                    description='two arguments are required'
-# )
-# parser.add_argument('config', metavar='CONFIG', action='store', help='the config to use')
-#
-# cmda_parser = subparsers.add_parser('cmdA',  help='a first command')
-# cmdb_parser = subparsers.add_parser('cmdB',  help='the second operation')
-# cmdc_parser = subparsers.add_parser('cmdC',  help='yet another thing')
-#
-# print(parser.parse_args())
-# ASK - How to reverse help help (--help, -h) and add -? <<<
+# ASK - Put optional args after required ones? <<<
 # http://superuser.com/questions/461946/can-i-use-pipe-output-as-a-shell-script-argument
 # ASK - How to test scripts in testing suite? <<<
 # From http://bioportal.weizmann.ac.il/course/python/PyMOTW/PyMOTW/docs/argparse/index.html to start
