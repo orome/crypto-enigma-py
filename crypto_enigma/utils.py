22f9b4bacbb0662d3c4de67218ff43cea9588f66
Roy Levien <royl@aldaron.com>
2015-11-24 17:00:22 -0500

    Add keyword argument handling to unicode decorator

--- crypto_enigma/utils.py
+++ crypto_enigma/utils.py
@@ -67,23 +67,32 @@ def chunk_of(it, n):
     return [it[i:i+n] for i in range(0, len(it), n)]
 
 
-# require unicode strings (see unicode_literal in enigma.py) - http://stackoverflow.com/a/33743668/656912
+# require unicode strings (see unicode_literal in enigma.py)
+#   http://stackoverflow.com/a/33743668/656912
+#   http://code.activestate.com/recipes/454322-type-checking-decorator/
 def require_unicode(*given_arg_names):
-    def check_types(_func_, *args):
-        def modified(*args):
+    def check_types(_func_, *args,  **kwargs):
+        def modified(*args, **kwargs):
             arg_names = list(_func_.func_code.co_varnames[:_func_.func_code.co_argcount])
             if len(given_arg_names) == 0:
                 unicode_arg_names = arg_names
+                #unicode_arg_names = arg_names
             else:
                 unicode_arg_names = given_arg_names
             for unicode_arg_name in unicode_arg_names:
                 try:
                     arg_index = arg_names.index(unicode_arg_name)
+                    if len(args) > arg_index:
+                        arg = args[arg_index]
+                    elif unicode_arg_name in kwargs:
+                        arg = kwargs[unicode_arg_name]
+                    else:
+                        # Not given as argument, even though in list
+                        continue
+                    if not isinstance(arg, unicode):
+                        raise TypeError("Parameter '{}' should be Unicode".format(unicode_arg_name))
                 except ValueError:
                     raise NameError(unicode_arg_name)
-                arg = args[arg_index]
-                if not isinstance(arg, unicode):
-                    raise TypeError("Parameter '{}' should be Unicode".format(unicode_arg_name))
-            return _func_(*args)
+            return _func_(*args, **kwargs)
         return modified
     return check_types
