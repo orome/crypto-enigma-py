a09c95eb216b18135e69345b3ac6b65148851be6
Roy Levien <royl@aldaron.com>
2015-12-04 09:54:17 -0500

    Configure pytest to handle Unicode in error reports

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,5 +5,12 @@ from __future__ import (absolute_import, print_function, division, unicode_liter
 from crypto_enigma import __version__
 
 
+# Fix errors when reporting Unicode
+def pytest_configure(config):
+    import sys
+    reload(sys)
+    sys.setdefaultencoding("UTF-8")
+
+
 def pytest_report_header(config):
     return "version: {}".format(__version__)
