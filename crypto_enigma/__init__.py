0c8f9b684b4cbb1f800b497d40696234a7fb951c
Roy Levien <royl@aldaron.com>
2015-12-22 10:35:37 -0500

    Tidy package init docs

--- crypto_enigma/__init__.py
+++ crypto_enigma/__init__.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 # encoding: utf8
 
-"""An Enigma machine simulator with rich textual display functionality.
+"""An Enigma machine simulator with rich textual display functionality for Python 2.7.
+
+Currently support is only provided for those `machine models`_ in most
+widespread general use during the war years: the `I`_, `M3`_, and `M4`_.
 
 Limitations
 ~~~~~~~~~~~
@@ -9,9 +12,9 @@ Limitations
 Note that the correct display of some characters used to represent
 components (thin Naval rotors) assumes support for Unicode, while some
 aspects of the display of machine state depend on support for combining
-Unicode. This is a `known
-limitation <https://github.com/orome/crypto-enigma-py/issues/1>`__ that
-will be addressed in a future release.
+Unicode.
+This is a `known limitation <https://github.com/orome/crypto-enigma-py/issues/1>`__
+that will be addressed in a future release.
 
 Note also that at the start of any scripts that use this package, you should
 
@@ -25,7 +28,7 @@ before any code that uses the API, or confiure IPython (in `ipython_config.py`) 
 
    c.InteractiveShellApp.exec_lines += ["from __future__ import unicode_literals"]
 
-or explicitly suppply Unicode strings (e.g., as in many of the examples here with :code:`u'TESTING'`).
+or explicitly supply Unicode strings (e.g., as in many of the examples here with :code:`u'TESTING'`).
 
 """
 
